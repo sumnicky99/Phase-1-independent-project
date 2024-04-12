30a6b2b864f4f4ddb7035f008671d1da8cc19800
Sumnicky99 <sumnicky99@gmail.com>
2024-04-12 17:43:30 +0300

    firstcommit

---
+++ phase-1-independent-project/index.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById("search").addEventListener("click", function(e) 
+{
+e.preventDefault()
+fetch("http://api.citybik.es/v2/networks")
+.then(response => 
+{
+if (!response.ok) {
+throw new Error("Failed to fetch data from the API");
+}
+return response.json(fetch);
+})
+.then(data => {
+// Process and display data
+console.log(data)
+displayData(data);
+})
+.catch(error => {
+console.error(error);
+});
+});
+function displayData(data) {
+const resultDiv = document.getElementById("search-result");
+//resultDiv.innerHTML = ""; // Clear previous results
+
+// Iterate over the networks and display information
+data.networks.forEach(network => {
+const networkDiv = document.createElement("div");
+networkDiv.textContent = network.name;
+resultDiv.appendChild(networkDiv);
+});
+}
+console.log(displayData)
+console.log(btn)
